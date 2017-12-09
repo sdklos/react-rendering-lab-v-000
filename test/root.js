dd4c01f1021727273d513b517c59dfd6ea08a8f3
sukey <sdklos@gmail.com>
2017-12-08 23:23:16 -0500

    Done.

--- test/root.js
+++ test/root.js
@@ -1,5 +1,9 @@
 require('babel-register')();
 
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
 var exposedProperties = ['window', 'navigator', 'document'];
 
 var jsdom = require('jsdom').jsdom;
